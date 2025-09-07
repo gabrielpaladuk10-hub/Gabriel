0192603b24b4ddabb59a04d69d2783a39202948e
gabrielpaladuk10-hub <gabrielpaladuk10@gmail.com>
2025-09-07 13:27:03 +0700

    Add files via upload

---
+++ Main (3).c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int length = 10;
+    int width = 7;
+    int area;
+
+    area = length * width;
+
+    printf("Length = %d\n", length);
+    printf("Width = %d\n", width);
+    printf("Area of rectangle = %d\n", area);
+
+    return 0;
+}
